2ae66bc0d59191c06fd1845944da428ee1bcb619
arjuncs8 <50917643+arjuncs8@users.noreply.github.com>
2021-02-24 14:25:46 +0530

    Add files via upload

---
+++ gal/gal.component.ts
@@ -0,0 +1,10 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-gal',
+  templateUrl: './gal.component.html',
+  styleUrls: ['./gal.component.css']
+})
+export class GalComponent  {
+
+}
